911f2bd73c12a281355d17bb9da8bf7142d153e0
LTailor <dzkhasayev@gmail.com>
2018-11-10 13:01:47 +0300

    fix display count and code style

--- report.jsx
+++ report.jsx
@@ -3,12 +3,18 @@ var ReactPivot = require('react-pivot')
 var createReactClass = require('create-react-class')
 var rows = require('./data.json')
 
+const fields = Object.keys(rows[0]);
+console.log(fields);
+
 module.exports = createReactClass({
   getInitialState: function() {
-    const fields = Object.keys(rows[0]);
-    console.log(fields);
-    const dims = fields.map((r)=>{return {title:r.toUpperCase(), value: r}});
+    const dims = fields.map((r) => {
+      return { 
+        title:r.toUpperCase(), 
+        value: r }
+    });
     console.log(dims);
+
     return {
       dims: dims,
       calculations:[ {
@@ -40,30 +46,30 @@ module.exports = createReactClass({
         template: function(val, row) {
           return (row.displays / row.loads * 100).toFixed(1) + ' %';
         }
-      }],
-      reduce: this.reduceScope()
-      };
-  },
-  reduceScope () {
-    let loads = {};
-    return (row, memo)=>{
-      if(!memo.loadss) {
-        memo.loadss = {};
-      }
-      memo.loads = (memo.loads || 0) + (memo.loadss[row.loadId] ? 0:1);
-      memo.displays = (memo.displays || 0) + (row.type === 'display' ? 1:0);
-      memo.impressions = (memo.impressions || 0) + (row.type === 'impression' ? 1:0)
-      memo.loadss[row.loadId] = true;
-      return memo;
-    }
+      }]
+    };
   },
   render () {
     return (<div>Report
       <ReactPivot rows={rows}
       dimensions={this.state.dims}
-      reduce = { this.state.reduce}
+      reduce = { this.reduce}
       calculations={this.state.calculations}
       nPaginateRows={25} 
       activeDimensions={['DATE']}/></div>)
+  },
+  reduce (row, memo) {
+    memo.earlyLoads = memo.earlyLoads || {};
+    memo.loadDisplays = memo.loadDisplays || {};
+    memo.loads = (memo.loads || 0) + (memo.earlyLoads[row.loadId] ? 0:1);
+    memo.earlyLoads[row.loadId] = true;
+    if (row.type === 'display') {
+      memo.displays = (memo.displays || 0) + (!memo.loadDisplays[row.loadId] ? 1:0);
+      memo.loadDisplays[row.loadId] = true;
+    }
+    
+    memo.impressions = (memo.impressions || 0) + (row.type === 'impression' ? 1:0)
+
+    return memo;
   }
 })
